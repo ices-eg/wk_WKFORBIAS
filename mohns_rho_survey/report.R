72e8be042af74872fd7a8cbc2d5487af121867a2
Colin Millar <colin.millar@ices.dk>
2019-10-31 14:27:31 +0100

    update data with scaled values

--- mohns_rho_survey/report.R
+++ mohns_rho_survey/report.R
@@ -10,23 +10,9 @@ mkdir("report")
 
 # read data
 
-mohns_raw <- read.csv("data/provided_stocks.csv")
+mohns <- read.csv("data/provided_stocks.csv")
 
-filter(mohns_raw, abs(ssb_rho) > 10 | abs(fbar_rho) > 10 | abs(rec_rho) > 10)
 
-perc_stocks <-
-  c("nop.27.3a4", 
-    "her.27.3031", "her.27.3a47d", "her.27.6a7bc",
-    "san.sa.3r",
-    "sol.27.20-24")
-
-mohns <- 
-  mohns_raw %>%
-  mutate(
-    ssb_rho = ifelse(stock %in% perc_stocks, ssb_rho / 100, ssb_rho),
-    fbar_rho = ifelse(stock %in% perc_stocks, fbar_rho / 100, fbar_rho),
-    rec_rho = ifelse(stock %in% perc_stocks, rec_rho / 100, rec_rho)
-    )
 
 
 

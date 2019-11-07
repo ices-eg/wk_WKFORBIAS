62200eaa6c21581a2dab25982b438039c23a490d
Colin Millar <colin.millar@ices.dk>
2019-11-07 11:29:19 +0100

    remove Bayesian models

--- mohns_rho_survey/data.R
+++ mohns_rho_survey/data.R
@@ -40,7 +40,7 @@ missing_cat_12_stocks <-
   missing_cat_12_stocks %>%
   filter(!substring(stock, 1, 3) %in% c("nep", "sal", "cap")) %>%
   filter(!stock %in% c("her.27.30", "dgs.27.nea", "bli.27.5b67")) %>%
-  filter(!grepl(".*SS3|Gadget.*", AssessmentType))
+  filter(!grepl(".*SS3|Gadget|CBBM|Bayesian.*", AssessmentType))
 
 write.taf(missing_cat_12_stocks, dir = "data", quote = TRUE)
 
